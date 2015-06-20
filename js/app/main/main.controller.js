86b76df00cd4cd68c86f19a60ea4ef824495e91f
Labsterx <labsterx@gmail.com>
2015-06-20 18:12:46 +0100

    more UI Changes

--- js/app/main/main.controller.js
+++ js/app/main/main.controller.js
@@ -4,5 +4,16 @@ angular.module('MyApp')
 	.controller('MainCtrl',
 		['$scope',
 		function ($scope) {
+
 			console.log('main controller');
+
+			$scope.input = {
+				companyUrl: null,
+			}
+
+			$scope.analyzeWithUrl = function() {
+				console.log('analyzeWithUrl', $scope.input.companyUrl);
+			}
+
+
 	}]);
